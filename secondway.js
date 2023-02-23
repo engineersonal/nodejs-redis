18774cf71a207d4842222c80250e59cd66925c04
engineersonal <engineersonal@gmail.com>
2023-02-23 15:11:10 +0100

    Added Singleton Redis Instance

---
+++ secondway.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const axios = require("axios");
+const redis = require("redis");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+//IIFE (Immediately Invoked Function Expression) returning back a Singleton redis instance
+const Singleton = (function () {
+    let redisInstance;
+
+    function createInstance() {
+        redisInstance = redis.createClient();
+
+        redisInstance.on("error", (error) => console.error(`Error : ${error}`));
+
+        redisInstance.connect();
+        return redisInstance;
+    }
+
+    return {
+        getInstance: function () {
+            if (!redisInstance) {
+                redisInstance = createInstance();
+            }
+            return redisInstance;
+        }
+    };
+  
+})();
+
+let redisClient = Singleton.getInstance();
+
+//Fetch the API Response
+async function fetchApiData(species) {
+  const apiResponse = await axios.get(
+    `https://www.fishwatch.gov/api/species/${species}`
+  );
+  console.log("Request sent to the API");
+  return apiResponse.data;
+}
+
+//Fetch the data from Redis Cache
+async function getCacheData(req, res, next) {
+  const species = req.params.species;
+  let results;
+  try {
+    const cacheResults = await redisClient.get(species);
+    if (cacheResults) {
+      results = JSON.parse(cacheResults);
+      res.send({
+        fromCache: true,
+        data: results,
+      });
+    } else {
+      next();
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(404);
+  }
+}
+
+//Fetch the data from API and store it in Redis Cache
+async function getSpeciesData(req, res) {
+  const species = req.params.species;
+  let results;
+
+  try {
+    results = await fetchApiData(species);
+    if (results.length === 0) {
+      throw "API returned an empty array";
+    }
+    await redisClient.set(species, JSON.stringify(results), {
+      EX: 180,
+      NX: true,
+    });
+
+    res.send({
+      fromCache: false,
+      data: results,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(404).send("Data unavailable");
+  }
+}
+
+//Add the middleware to check for the cached Redis data
+app.get("/fish/:species", getCacheData, getSpeciesData);
+
+//Application listens on a port
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
